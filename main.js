70772c5429e7f8ad26982e0e11d2f55ed5db1cc6
TheWord Blog <blogoftheword@gmail.com>
2023-06-05 23:17:43 +0300

    Co-authored-by: JHawi13 <JHawi13@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-// Grab elements
+window.onload = function() {
+  // Grab elements
 const selectElement = (selector) => {
-    const element = document.querySelector(selector);
-    if(element) return element;
-    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+  const element = document.querySelector(selector);
+  if(element) return element;
+  throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
 };
 
 //Nav styles on scroll
 const scrollHeader = () =>{
-    const navbarElement = selectElement('#header'); 
-    if(this.scrollY >= 15) {
-        navbarElement.classList.add('activated');
-    } else {
-        navbarElement.classList.remove('activated');
-    }
+const navbarElement = selectElement('#header'); 
+if(this.scrollY >= 15) {
+    navbarElement.classList.add('activated');
+} else {
+    navbarElement.classList.remove('activated');
+}
 }
 
 window.addEventListener('scroll', scrollHeader);
@@ -21,9 +22,9 @@ window.addEventListener('scroll', scrollHeader);
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 
 const toggleMenu = () =>{
-    const mobileMenu = selectElement('#menu');
-    mobileMenu.classList.toggle('activated');
-    menuToggleIcon.classList.toggle('activated');
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu);
@@ -39,102 +40,105 @@ const currentTheme = localStorage.getItem('currentTheme');
 
 // Check to see if there is a theme preference in local Storage, if so add the light theme to the body
 if (currentTheme) {
-    body.classList.add('light-theme');
+  body.classList.add('light-theme');
 }
 
 themeToggleBtn.addEventListener('click', function () {
-    // Add light theme on click
-    body.classList.toggle('light-theme');
+  // Add light theme on click
+  body.classList.toggle('light-theme');
 
-    // If the body has the class of light theme then add it to local Storage, if not remove it
-    if (body.classList.contains('light-theme')) {
-        localStorage.setItem('currentTheme', 'themeActive');
-    } else {
-        localStorage.removeItem('currentTheme');
-    }
+  // If the body has the class of light theme then add it to local Storage, if not remove it
+  if (body.classList.contains('light-theme')) {
+      localStorage.setItem('currentTheme', 'themeActive');
+  } else {
+      localStorage.removeItem('currentTheme');
+  }
 });
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+var i;
+var slides = document.getElementsByClassName("slide");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+slides[slideIndex-1].style.display = "block";
 }
 
 function automateScrolling() {
-    const slides = document.querySelectorAll('.product-slide');
-    const interval = 2000; // Set the interval time in milliseconds (e.g., 2000ms = 2 seconds)
-    let currentSlide = 0;
-  
-    function showSlide(index) {
-      slides.forEach((slide) => {
-        slide.style.display = 'none';
-      });
-  
-      slides[index].style.display = 'block';
-    }
-  
-    function nextSlide() {
-      currentSlide++;
-      if (currentSlide >= slides.length) {
-        currentSlide = 0;
-      }
-      showSlide(currentSlide);
-    }
-  
-    setInterval(nextSlide, interval);
-  }
-  
-  document.addEventListener('DOMContentLoaded', automateScrolling);
+  const slides = document.querySelectorAll('.product-slide');
+  const interval = 2000; // Set the interval time in milliseconds (e.g., 2000ms = 2 seconds)
+  let currentSlide = 0;
 
-  // Smooth scrolling function
-  function smoothScroll(target, duration) {
-    var targetElement = document.querySelector(target);
-    var targetPosition = targetElement.offsetTop;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-
-    function animation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      var timeElapsed = currentTime - startTime;
-      var scrollAmount = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, scrollAmount);
-      if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    // Easing function for smooth scrolling effect
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-  }
-
-  // Attach click event listeners to the navigation links
-  var navLinks = document.querySelectorAll('nav a');
-  navLinks.forEach(function (link) {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      var target = this.getAttribute('href');
-      var duration = 1000; // Adjust duration (in milliseconds) as desired
-      smoothScroll(target, duration);
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.style.display = 'none';
     });
+
+    slides[index].style.display = 'block';
+  }
+
+  function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+    showSlide(currentSlide);
+  }
+
+  setInterval(nextSlide, interval);
+}
+
+document.addEventListener('DOMContentLoaded', automateScrolling);
+
+// Smooth scrolling function
+function smoothScroll(target, duration) {
+  var targetElement = document.querySelector(target);
+  var targetPosition = targetElement.offsetTop;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var scrollAmount = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, scrollAmount);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing function for smooth scrolling effect
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Attach click event listeners to the navigation links
+var navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    var target = this.getAttribute('href');
+    var duration = 1000; // Adjust duration (in milliseconds) as desired
+    smoothScroll(target, duration);
   });
+});
 
 
-  
+
+
+
+}
