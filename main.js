6c4981ca913f900a15dc9f1c320b96e3dacf49f9
TheWord Blog <blogoftheword@gmail.com>
2023-06-06 12:38:37 +0300

    Fixing broken slideshow

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-window.onload = function() {
-  // Grab elements
+// Grab elements
 const selectElement = (selector) => {
   const element = document.querySelector(selector);
   if(element) return element;
@@ -8,12 +7,12 @@ const selectElement = (selector) => {
 
 //Nav styles on scroll
 const scrollHeader = () =>{
-const navbarElement = selectElement('#header'); 
-if(this.scrollY >= 15) {
-    navbarElement.classList.add('activated');
-} else {
-    navbarElement.classList.remove('activated');
-}
+  const navbarElement = selectElement('#header');
+  if(this.scrollY >= 15) {
+      navbarElement.classList.add('activated');
+  } else {
+      navbarElement.classList.remove('activated');
+  }
 }
 
 window.addEventListener('scroll', scrollHeader);
@@ -28,20 +27,6 @@ const toggleMenu = () =>{
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu);
-
-// Get the first element with the class name "my-link"
-var element = document.getElementsByClassName("list-link");
-
-// Add a click event listener to the element
-element.addEventListener("click", function(event) {
-  // Prevent the default behavior of the link
-  event.preventDefault();
-  // Get the href attribute of the element
-  var href = element.getAttribute("href");
-  // Redirect the page to the href value
-  window.location.href = href;
-});
-
 
 
 
@@ -69,23 +54,25 @@ themeToggleBtn.addEventListener('click', function () {
   }
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-showSlides(slideIndex += n);
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+function showSlides() {
+for (let i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > slides.length) {
+  slideIndex = 1;
+}
+slides[slideIndex - 1].style.display = "block";
+setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 
-function showSlides(n) {
-var i;
-var slides = document.getElementsByClassName("slide");
-if (n > slides.length) {slideIndex = 1}
-if (n < 1) {slideIndex = slides.length}
-for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-}
-slides[slideIndex-1].style.display = "block";
-}
+showSlides();
 
 function automateScrolling() {
   const slides = document.querySelectorAll('.product-slide');
@@ -99,7 +86,6 @@ function automateScrolling() {
 
     slides[index].style.display = 'block';
   }
-
   function nextSlide() {
     currentSlide++;
     if (currentSlide >= slides.length) {
@@ -112,5 +98,3 @@ function automateScrolling() {
 }
 
 document.addEventListener('DOMContentLoaded', automateScrolling);
-
-};
